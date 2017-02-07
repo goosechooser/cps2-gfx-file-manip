48286f69ae54495f6ee2bb579a256ea74f72b05a
Marshall <xtremebass2k@gmail.com>
2017-02-07 05:27:10 -0800

    Abstract class others inherit from

--- cps2_file_manip/FileFormat.py
+++ cps2_file_manip/FileFormat.py
@@ -3,6 +3,10 @@ from abc import ABCMeta, abstractmethod
 class FileFormat(object):
     __metaclass__ = ABCMeta
 
+    def __init__(self, verbose):
+        self._verbose = verbose
+        self.verboseprint = print if self._verbose else lambda *a, **k: None
+
     @abstractmethod
     def interleave_files(self):
         pass
