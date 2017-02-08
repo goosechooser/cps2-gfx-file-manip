0967de3b806176e444265a7ac01e9d59318f8b36
Marshall <xtremebass2k@gmail.com>
2017-02-08 06:26:20 -0800

    (de)interleave functions now accept lists

--- cps2_file_manip/file_manip.py
+++ cps2_file_manip/file_manip.py
@@ -1,35 +1,51 @@
 from struct import Struct
 
-def deinterleave(data, num_bytes):
-    """Deinterleaves a bytearray.
+#all i ask for is "interleave {files here} bytesize" 
+#and the opposite "deinterleave interleavedfile bytesize numberoffiles"
+#example midway unit T
+#functions should accept a list, and numbytes to (de)interleave by
 
-    Returns two bytearrays.
+def deinterleave(data, nbytes, nsplit):
+    """Deinterleaves one bytearray into nsplit many bytearrays on a nbytes basis.
+
+    Returns a list of bytearrays.
     """
-    evens = []
-    odds = []
-    deinterleave_s = Struct('c' * num_bytes)
+    deinterleaved = [[] for n in range(nsplit)]
+
+    deinterleave_s = Struct('c' * nbytes)
     deinterleave_iter = deinterleave_s.iter_unpack(data)
 
     for i in deinterleave_iter:
-        evens.extend([*i])
-        odds.extend([*next(deinterleave_iter)])
+        deinterleaved[0].extend([*i])
+        for j in range(1, len(deinterleaved[1:]) + 1):
+            next_ = next(deinterleave_iter)
+            deinterleaved[j].extend([*next_])
 
-    return b''.join(evens), b''.join(odds)
+    return [b''.join(delist) for delist in deinterleaved]
 
-def interleave(file1, file2, num_bytes):
-    """Interleaves two bytearray buffers together.
+def interleave(data, nbytes):
+    """Interleaves a list of bytearrays together on a nbytes basis.
 
     Returns a bytearray.
     """
+    interleave_s = Struct('c' * nbytes)
+    iters = [interleave_s.iter_unpack(inter) for inter in data]
+
     interleaved = []
-    interleave_s = Struct('c' * num_bytes)
-    file1_iter = interleave_s.iter_unpack(file1)
-    file2_iter = interleave_s.iter_unpack(file2)
+    #this could cause rounding errors?
+    iterlen = int(len(data[0]) / nbytes)
+    for i in range(iterlen):
+        nexts = [next(iter_) for iter_ in iters]
+        # print('nexts is:', nexts)
+        interleaved.extend([b''.join(val) for val in nexts])
+        # print('interleaved is:', interleaved)
 
-    for i in file1_iter:
-        file2_next = next(file2_iter)
-        interleave_temp = [*i, *file2_next]
-        interleaved.extend(interleave_temp)
+    return b''.join(interleaved)
 
-    return  b''.join(interleaved)
-            
+if __name__ == '__main__':
+    data1 = bytearray.fromhex('AA AA AA AA BB BB BB BB CC CC CC CC DD DD DD DD')
+    result = deinterleave(data1, 1, 8)
+    #print(result)
+    #newresult = interleave(result, 2)
+    #print(newresult)
+
