8fee7eecf34a634e361eb530db864d6566b93883
Marshall <xtremebass2k@gmail.com>
2017-02-10 04:19:26 -0800

    Refactored saving into its own function

--- cps2_file_manip/CustomFormat.py
+++ cps2_file_manip/CustomFormat.py
@@ -9,27 +9,12 @@ class CustomFormat(FileFormat):
         data = [self.open_file(fp) for fp in self._filepaths]
 
         self.verboseprint('Interleaving files every', self._numbytes, 'bytes')
-        interleave_data = interleave(data, self._numbytes)
+        interleave_data = [interleave(data, self._numbytes)]
 
-        #if no custom output, save to cwd with default name
-        if not self._savepaths:
-            fnames = [os.path.split(fname)[1] for fname in self._filepaths]
-            spath = '.'.join([*fnames, 'combined'])
+        filename = ['.'.join([os.path.split(fname)[1] for fname in self._filepaths])]
+        suffix = ['combined']
 
-        #if custom output is a folder, save default file name to that location
-        elif os.path.isdir(self._savepaths):
-            head = self._savepaths
-            fnames = [os.path.split(fname)[1] for fname in self._filepaths]
-            tail = '.'.join([*fnames, 'combined'])
-            spath = os.path.join(head, tail)
-
-        #else use given output
-        else:
-            spath = self._savepaths
-
-        with open(spath, 'wb') as f:
-            self.verboseprint('Saving', spath)
-            f.write(interleave_data)
+        self.save(interleave_data, filename, suffix)
 
     def deinterleave_file(self):
         self.verboseprint('Opening file')
@@ -39,54 +24,41 @@ class CustomFormat(FileFormat):
         self.verboseprint('Producing', self._nsplit, 'files')
         deinterleave_data = deinterleave(fdata, self._numbytes, self._nsplit)
 
-        #if no custom output, save to cwd with default name
-        if not self._savepaths:
-            fname = [os.path.split(fname)[1] for fname in self._filepaths]
-            spaths = ['.'.join([fname[0], str(i)]) for i in range(self._nsplit)]
+        filenames = [os.path.split(self._filepaths[0])[1]] * self._nsplit
+        suffixes = [str(i) for i in range(self._nsplit)]
 
-        #if custom output is a folder, save default file name to that location
-        elif os.path.isdir(self._savepaths):
-            head = self._savepaths
-            fname = os.path.split(self._filepaths[0])[1]
-            tails = ['.'.join([fname, str(i)]) for i in range(self._nsplit)]
-            spaths = [os.path.join(head, tail) for tail in tails]
-
-        #if custom output is a file, append number to the end of it
-        else:
-            spaths = ['.'.join([self._savepaths, str(i)]) for i in range(self._nsplit)]
-
-            #investigate whether multiple custom output names is necessary
-            # else:
-            #     spaths = ['.'.join([self._savepaths[i], str(i)]) for i in range(self._nsplit)]
-
-        for i, savepath in enumerate(spaths):
-            with open(savepath, 'wb') as f:
-                self.verboseprint('Saving', savepath)
-                f.write(deinterleave_data[i])
+        self.save(deinterleave_data, filenames, suffixes)
 
     def bswap_file(self, fmt):
         self.verboseprint('Opening file')
         fdata = self.open_file(self._filepaths[0])
 
         self.verboseprint('Byte swapping file every', fmt, 'bytes')
-        swapped = swap(fdata, fmt)
+        swapped = [swap(fdata, fmt)]
+        filename = ['.'.join([os.path.split(fname)[1] for fname in self._filepaths])]
+        suffix = ['.swapped']
 
+        self.save(swapped, filename, suffix)
+
+    def save(self, savedata, filenames, suffixes):
         #if no custom output, save to cwd with default name
         if not self._savepaths:
-            fnames = [os.path.split(fname)[1] for fname in self._filepaths]
-            spath = '.'.join([*fnames, 'swapped'])
+            fnames = [os.path.split(fname)[1] for fname in filenames]
+            spaths = ['.'.join([fname, s]) for fname, s in zip(fnames, suffixes)]
 
         #if custom output is a folder, save default file name to that location
         elif os.path.isdir(self._savepaths):
             head = self._savepaths
-            fnames = [os.path.split(fname)[1] for fname in self._filepaths]
-            tail = '.'.join([*fnames, 'swapped'])
-            spath = os.path.join(head, tail)
+            print('filenames is:', filenames)
+            tails = ['.'.join([fname, s]) for fname, s in zip(filenames, suffixes)]
+            print('tails is:', tails)
+            spaths = [os.path.join(head, tail) for tail in tails]
 
-        #else use given output
+        #if custom output is a file, append number to the end of it
         else:
-            spath = self._savepaths
+            spaths = ['.'.join([self._savepaths, s]) for s in suffixes]
 
-        with open(spath, 'wb') as f:
-            self.verboseprint('Saving', spath)
-            f.write(swapped)
+        for savepath, data in zip(spaths, savedata):
+            with open(savepath, 'wb') as f:
+                self.verboseprint('Saving', savepath)
+                f.write(data)
